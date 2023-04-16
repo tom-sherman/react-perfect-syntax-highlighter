8a015ab2b471bdee7f191f2fea708f61aa8fa72f
Tom Sherman <the.tomsherman@gmail.com>
2023-04-16 11:11:21 +0100

    Try outputFileTracingIncludes

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-    serverComponentsExternalPackages: ["shiki"],
+    outputFileTracingIncludes: ["lib/shiki"],
   },
   typescript: {
     ignoreBuildErrors: true,

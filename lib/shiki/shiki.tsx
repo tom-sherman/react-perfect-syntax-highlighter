20f35117c7f04c8c853149085bd7efd45bbf548e
Tom Sherman <the.tomsherman@gmail.com>
2023-05-13 19:45:24 +0100

    Try @code-hike/lighter

--- lib/shiki/shiki.tsx
+++ lib/shiki/shiki.tsx
@@ -1,59 +1,37 @@
 import "server-only";
-import {
-  BUNDLED_LANGUAGES,
-  FontStyle,
-  getHighlighter as shikiGetHighlighter,
-} from "shiki";
-import { CSSProperties, Fragment, cache } from "react";
+import { Theme, highlight as lighterHighlight } from "@code-hike/lighter";
+import { Fragment, cache } from "react";
 import clsx from "clsx";
-import { NEXT_BUNDLED_THEMES } from "./themes";
 
 export interface ShikiProps {
   code: string;
   lang: string;
-  theme: string;
+  theme: Theme;
   className?: string;
 }
 
 export async function Shiki({ code, lang, theme, className }: ShikiProps) {
-  const highlighter = await getHighlighter(lang, theme);
-  const tokens = highlighter.codeToThemedTokens(code, lang, theme);
-  const background = highlighter.getBackgroundColor(theme);
+  const { lines, colors } = await highlight(code, lang, theme);
 
   return (
     <pre
       className={clsx("shiki overflow-x-auto p-4 rounded", lang, className)}
-      style={{ backgroundColor: background }}
+      style={{ backgroundColor: colors.background }}
       tabIndex={0}
     >
       <code>
-        {tokens.map((tokenLine, i) => (
+        {lines.map((tokenLine, i) => (
           <Fragment key={i}>
             <span className="line">
               {tokenLine.map((token, j) => {
-                const style: CSSProperties = {
-                  color: token.color,
-                };
-                if (token.fontStyle) {
-                  if (token.fontStyle & FontStyle.Italic) {
-                    style["fontStyle"] = "italic";
-                  }
-                  if (token.fontStyle & FontStyle.Bold) {
-                    style["fontWeight"] = "bold";
-                  }
-                  if (token.fontStyle & FontStyle.Underline) {
-                    style["textDecoration"] = "underline";
-                  }
-                }
-
                 return (
-                  <span key={j} style={style}>
+                  <span key={j} style={token.style}>
                     {token.content}
                   </span>
                 );
               })}
             </span>
-            {i < tokens.length - 1 && "\n"}
+            {i < lines.length - 1 && "\n"}
           </Fragment>
         ))}
       </code>
@@ -61,39 +39,9 @@ export async function Shiki({ code, lang, theme, className }: ShikiProps) {
   );
 }
 
-export async function tokenizeCode(code: string, lang: string, theme: string) {
+export async function tokenizeCode(code: string, lang: string, theme: Theme) {
   "use server";
   return <Shiki code={code} lang={lang} theme={theme} />;
 }
 
-const highlighterPromise = shikiGetHighlighter({});
-
-const getHighlighter = cache(async (language: string, theme: string) => {
-  console.log("Loading highlighter", language, theme);
-  const highlighter = await highlighterPromise;
-  const loadedLanguages = highlighter.getLoadedLanguages();
-  const loadedThemes = highlighter.getLoadedThemes();
-
-  let promises = [];
-  if (!loadedLanguages.includes(language as any)) {
-    const languageRegistration = BUNDLED_LANGUAGES.find(
-      (l) => l.id === language
-    );
-    if (!languageRegistration) {
-      throw new Error(`Language not found: ${language}`);
-    }
-    promises.push(highlighter.loadLanguage(languageRegistration));
-  }
-
-  if (!loadedThemes.includes(theme as any)) {
-    const themeRegistration = NEXT_BUNDLED_THEMES.find((t) => t.name === theme);
-    if (!themeRegistration) {
-      throw new Error(`Theme not found: ${theme}`);
-    }
-    promises.push(highlighter.loadTheme(themeRegistration));
-  }
-
-  await Promise.all(promises);
-
-  return highlighter;
-});
+const highlight = cache(lighterHighlight);
